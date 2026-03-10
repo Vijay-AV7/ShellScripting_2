6a16c9a572ceecdbba140d31f55bc4697081425d
Vijay <gkpk222@gmail.com>
2026-03-10 20:14:30 +0530

    Arrays

---
+++ 07-Arrays.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TIMESTAMP_START=$(date)
+
+echo "Script execution started at : "$TIMESTAMP_START""
+
+MOVIES=(Dhurandhar, Peddi, AA22)
+
+echo "First Movie : $MOVIES[0]"
+echo "Second Movie : $MOVIES[1]"
+echo "Third Movie : $MOVIES[2]"
+echo "All movies : $MOVIES[@]"
+
+TIMESTAMP_END=$(date)
+echo "Script execution end at : "$TIMESTAMP_END""
