9ecaa239ed5fbb6d262ac731b2cdfba3739e3058
Vijay <gkpk222@gmail.com>
2026-03-10 21:04:46 +0530

    Arrays

---
+++ 09-Application_Install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TIMESTAMP_START=$(date)
+USER_ID=$(id -u)
+
+if ( $USER_ID -nq 0 )
+then  
+        echo "ERROR:: You must have the sudo access to execute this script"
+        exit 1
+else 
+        echo "$USER user started executing the script at : $TIMESTAMP_START"
+fi
+
+dnf list installed mysql
+
+if ( $? eq 0 )
+then 
+    echo "Mysql .... already installed" 
+else
+    dnf install mysql -y
+    if ( $? eq 0 )
+    then 
+        echo "Mysql .... installed successfully" 
+    else    
+        echo "ERROR:: Mysql .... installed Failure"
+        exit 1
+    fi
+fi    
