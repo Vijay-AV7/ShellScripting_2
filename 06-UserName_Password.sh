0a6b36872018f7468ae6a73467c58ad97d04cbbd
Vijay <gkpk222@gmail.com>
2026-03-10 17:34:58 +0530

    LoginSetup

---
+++ 06-UserName_Password.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DBUSERNAME=Vijay@32
+DBPASSWORD=123
+
+echo "Please enter you username :"
+read USERNAME
+echo "USERNAME entered : $USERNAME"
+echo "Please enter your password :"
+read -s PASSWORD
+
+if [ $DBUSERNAME -eq $USERNAME ]
+then
+    if [ $DBPASSWORD -eq $PASSWORD ]
+    then
+        echo "Log in successful ...."
+    else
+        echo "incorrect Password"
+    fi
+else
+        echo "Incorrect Username"
+fi
