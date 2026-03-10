c88079a8b1465a7e12f8a64edd03f2434b824e9f
Vijay <gkpk222@gmail.com>
2026-03-10 18:44:43 +0530

    LoginSetup

--- 06-UserName_Password.sh
+++ 06-UserName_Password.sh
@@ -9,17 +9,27 @@ echo "USERNAME entered : $USERNAME"
 echo "Please enter your password :"
 read -s PASSWORD
 
-#If we are comparing the strings use "==" for equal and "!=" for not equal
+#If we are comparing the strings use below
+# == or = → equal 
+# != → not equal
+# < → less than (in ASCII order)
+# > → greater than (in ASCII order)
+# -z STRING → true if string is empty
+# -n STRING → true if string is not empty
 
-
-if [ $DBUSERNAME == $USERNAME ]
+if [ -n $USERNAME ]
 then
-    if [ $DBPASSWORD -eq $PASSWORD ]
+    if [ $DBUSERNAME == $USERNAME ]
     then
-        echo "Log in successful ...."
+        if [ $DBPASSWORD -eq $PASSWORD ]
+        then
+            echo "Log in successful ...."
+        else
+            echo "incorrect Password"
+        fi
     else
-        echo "incorrect Password"
+            echo "Incorrect Username"
     fi
-else
-        echo "Incorrect Username"
+else 
+    echo "No username is entered, "
 fi
