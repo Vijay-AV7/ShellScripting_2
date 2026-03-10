d8be2983bfed6e30bcd5f694b468a48647642a6e
Vijay <gkpk222@gmail.com>
2026-03-10 17:14:52 +0530

    Comment added :- #NAME1, NAME2 are two variables

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-NAME1=Rahim
+NAME1=Rahim 
 NAME2=Robert
 
-echo "$NAME1:: Hi $NAME2"
+#NAME1, NAME2 are two variables
+
+echo "$NAME1:: Hi ${NAME2}"
 echo "$NAME2:: Hello $NAME1"
 echo "$NAME1:: How are you doing?"
 echo "$NAME2:: I am good. How are you?"
