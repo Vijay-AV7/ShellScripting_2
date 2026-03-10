35c06c019c3d968e2197fd085ea9c560e5f482e3
Vijay <gkpk222@gmail.com>
2026-03-10 21:15:59 +0530

    Arrays

--- 09-Application_Install.sh
+++ 09-Application_Install.sh
@@ -15,7 +15,7 @@ dnf list installed mysql
 
 if [ $? -eq 0 ]
 then 
-    echo "Mysql .... already installed" 
+    echo "INFORMATION:: Mysql .... already installed" 
 else
     dnf install mysql -y
     if [ $? -eq 0 ]
@@ -25,4 +25,20 @@ else
         echo "ERROR:: Mysql .... installed Failure"
         exit 1
     fi
-fi    
+fi  
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Git ... FAILURE"
+        exit 1
+    else
+        echo "Installing Git ... SUCCESS"
+    fi
+else
+    echo "Git is already ... INSTALLED"
+fi
