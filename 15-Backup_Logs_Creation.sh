28b709b75ba2b236788d10f30f74b5fd41b49c67
Vijay <gkpk222@gmail.com>
2026-03-12 12:44:18 +0530

    14,15 files are added

--- 15-Backup_Logs_Creation.sh
+++ 15-Backup_Logs_Creation.sh
@@ -3,7 +3,7 @@
 TIMESTAMP=$(date)
 mkdir -p /var/log/shellscript_logs
 LOGDIR="/var/log/shellscript_logs"
-FILENAME=$(echo "$0" | cut -d "." -f1)
+FILENAME=$(echo $0 | cut -d "." -f1)
 LOGNAME="$LOGDIR/$FILENAME_$TIMESTAMP.log"
 touch $LOGNAME
 cd $LOGDIR
