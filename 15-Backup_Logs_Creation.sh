35789dc0617fdbbf2438aaed9afeb576776919d3
Vijay <gkpk222@gmail.com>
2026-03-13 00:14:53 +0530

    14,15 files are added

--- 15-Backup_Logs_Creation.sh
+++ 15-Backup_Logs_Creation.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+TIMESTAMP=$(date +%Y/%m/%d_%H_%M_%S)
 mkdir -p /var/log/shellscript_logs
 LOGDIR="/var/log/shellscript_logs"
 FILENAME=$(echo $0 | cut -d "." -f1)
-LOGNAME="$LOGDIR/$FILENAME_$TIMESTAMP.log"
-touch $LOGNAME
-cd $LOGDIR
-ls -l
+LOG_FILENAME="$LOGDIR/$FILENAME_$TIMESTAMP.log"
+echo "Log file name is : $LOG_FILENAME" 
+
+SOURCE_DIR=$LOGDIR
+ARCIEVE_DIR="/home/ec2-user/archieve_logs"
+
+INSTALL_VALIDATE () {
+
+dnf list installed $1 &>>$LOG_FILENAME
+if [ $? -eq 0 ]
+then
+    echo "$1 is already installed"
+else
+    dnf install $1 -y &>>$LOG_FILENAME
+    if [ $? -eq 0 ]
+    then
+        echo "$1 installation .... successful"
+    else
+        echo " .$1 installation .... failure"
+        exit 1
+    fi
+fi 
+}
+
+INSTALL_VALIDATE mysql 
+
 
